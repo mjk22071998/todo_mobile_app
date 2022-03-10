239ca8a328d4275abea5ce010a6f102cc4d21834
muhammadjunaidkhalid <mjk22071998@gmail.com>
2022-03-10 23:30:04 +0500

    UI started

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,39 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Todo List"),
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(5),
+                decoration: const BoxDecoration(color: Colors.indigoAccent),
+                child: Row(
+                  children: [
+                    const Expanded(
+                        child: TextField(
+                      decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.black,
+                          labelText: "Enter To Do Task title"),
+                    )),
+                    ElevatedButton.icon(
+                      onPressed: () {},
+                      icon: const Icon(Icons.add),
+                      label: const Text("Add Task"),
+                      style: ElevatedButton.styleFrom(
+                        shape: const StadiumBorder()
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
