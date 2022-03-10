a37c5b5cf5af85b804e2110e05bdd333a6430408
muhammadjunaidkhalid <mjk22071998@gmail.com>
2022-03-10 22:57:13 +0500

    delete task function added in DBHelper

--- lib/database/db_helper.dart
+++ lib/database/db_helper.dart
@@ -40,6 +40,7 @@ class DBHelper {
     db.insert(tableName, todoTask.toMap(),
         conflictAlgorithm: ConflictAlgorithm.replace);
   }
+  
   Future<dynamic> getAllTasks() async {
     final db = await database;
     List<Map> res = await db.query(tableName);
@@ -49,4 +50,12 @@ class DBHelper {
       return res.toList();
     }
   }
+
+  Future<int> deleteNote(int id) async {
+    final db = await database;
+    int count = await
+        db.rawDelete('''DELETE FROM $tableName WHERE id = $id''');
+    return count;
+  }
+
 }
